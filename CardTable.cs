e95a8a6bafd88592fcf08e178a62ca00dc62bc4c
DGM6308 <DGM6308@labB2QOI1>
2024-02-13 02:20:43 +0000

    Add project files.

---
+++ CardTable.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaceTo21
+{
+    public class CardTable
+    {
+        public CardTable()
+        {
+            Console.WriteLine("Setting Up Table...");
+        }
+
+        /* Shows the name of each player and introduces them by table position.
+         * Is called by Game object.
+         * Game object provides list of players.
+         * Calls Introduce method on each player object.
+         */
+        public void ShowPlayers(List<Player> players)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].Introduce(i+1); // List is 0-indexed but user-friendly player positions would start with 1...
+            }
+        }
+
+        /* Gets the user input for number of players.
+         * Is called by Game object.
+         * Returns number of players to Game object.
+         */
+
+        public bool OfferACard(Player player)
+        {
+            Console.Write($"{player.name}, do you want to draw a card? (Y/N) ");
+            string response = Console.ReadLine();
+            return response.ToUpper().StartsWith("Y");
+        }
+        public int GetNumberOfPlayers()
+        {
+            Console.Write("How many players? ");
+            string response = Console.ReadLine();
+            int numberOfPlayers;
+            while (int.TryParse(response, out numberOfPlayers) == false
+                || numberOfPlayers < 2 || numberOfPlayers > 8)
+            {
+                Console.WriteLine("Invalid number of players.");
+                Console.Write("How many players?");
+                response = Console.ReadLine();
+            }
+            return numberOfPlayers;
+        }
+
+        /* Gets the name of a player
+         * Is called by Game object
+         * Game object provides player number
+         * Returns name of a player to Game object
+         */
+        public string GetPlayerName(int playerNum)
+        {
+            Console.Write("What is the name of player# " + playerNum + "? ");
+            string response = Console.ReadLine();
+            while (response.Length < 1)
+            {
+                Console.WriteLine("Invalid name.");
+                Console.Write("What is the name of player# " + playerNum + "? ");
+                response = Console.ReadLine();
+            }
+            return response;
+        }
+
+
+
+
+
+        public int AskNumberOfCards(Player player)
+        {
+            Console.WriteLine($"{player.name}, how many cards do you want to draw (up to 3)? ");
+            int numberOfCards;
+            while (!int.TryParse(Console.ReadLine(), out numberOfCards) || numberOfCards < 1 || numberOfCards > 3)
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
+            }
+            return numberOfCards;
+        }
+
+        public void ShowHand(Player player)
+        {
+            if (player.cards.Count > 0)
+            {
+                Console.Write(player.name + " has: ");
+                foreach (Card card in player.cards)
+                {
+                    Console.Write(card.name + " ");
+                }
+                Console.Write("=" + player.score + "/21 ");
+                if (player.status != PlayerStatus.active)
+                {
+                    Console.Write("(" + player.status.ToString().ToUpper() + ")");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowHands(List<Player> players)
+        {
+            foreach (Player player in players)
+            {
+                ShowHand(player);
+            }
+        }
+
+
+        public void AnnounceWinner(Player player)
+        {
+            if (player != null)
+            {
+                Console.WriteLine(player.name + " wins!");
+            }
+            else
+            {
+                Console.WriteLine("Everyone busted!");
+            }
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+
+    }
+}
